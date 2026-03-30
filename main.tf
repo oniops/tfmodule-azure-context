91de5db1598a98da51c3f2d5d04458486e0f981a
hoseong.son <hoseong.son@opsnow.com>
2026-03-30 20:19:04 +0900

    OI-738 Azure - 컨텍스트 값 보완

--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
 locals {
-  environment = lower(var.context.environment)
-  env_alias   = substr(local.environment, 0, 1)
-  owner       = var.owner != null ? var.owner : var.context.owner
+  environment  = lower(var.context.environment)
+  env_alias    = substr(local.environment, 0, 1)
+  region_alias = lookup(local.az_region_codes, var.context.region, "nn")
+  owner        = var.owner != null ? var.owner : var.context.owner
 
   local_tags = {
-    Project     = var.context.project
-    Environment = var.context.environment
-    Department  = var.context.department
-    Owner       = local.owner
-    Customer    = var.context.customer
-    ManagedBy   = var.provisioner
+    project     = var.context.project
+    environment = var.context.environment
+    department  = var.context.department
+    owner       = local.owner
+    customer    = var.context.customer
+    managed-by  = var.provisioner
   }
 
+  name_prefix = "${var.context.project}-${local.region_alias}${local.env_alias}"
+
   tags = merge(
-    (var.team != null ? {
-      Team = var.team
-    } : {}),
+    (var.cost_center != null ? { cost-center = var.cost_center } : {}),
+    (var.team != null ? { Team = var.team } : {}),
     local.local_tags,
     var.additional_tags
   )
+
+  az_region_codes = {
+    koreacentral = "kc"
+  }
 }
