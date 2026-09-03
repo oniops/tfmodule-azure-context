239b8afcec0cdaa838e93bbfa0d64d40a40b51d6
hoseong.son <hoseong.son@opsnow.com>
2026-09-03 11:02:21 +0900

    OI-1160 IaC - context에 subscription_id 추가 및 오타 등 마이너 버그 수정

--- variables.tf
+++ variables.tf
@@ -1,13 +1,14 @@
 variable "context" {
   type = object({
-    project     = string # project name is usally account's project name or platform name
-    region      = string # describe default region to create a resource
-    environment = string # Distinguish between development, test, and production resources
-    department  = string # Organization name of BSP
-    owner       = string # Identify who is responsible for the resource
-    customer    = string # Identify a specific client that a particular group of resources serves
-    domain      = string # public toolchain domain name (ex, tools.customer.co.kr)
-    tenant_id   = string # Azure Tenant ID
+    project         = string # project name is usually account's project name or platform name
+    region          = string # describe default region to create a resource
+    environment     = string # Distinguish between development, test, and production resources
+    department      = string # Organization name of BSP
+    owner           = string # Identify who is responsible for the resource
+    customer        = string # Identify a specific client that a particular group of resources serves
+    domain          = string # public toolchain domain name (ex, tools.customer.co.kr)
+    tenant_id       = string # Azure Tenant ID
+    subscription_id = string # Azure Subscription ID
   })
 }
 
